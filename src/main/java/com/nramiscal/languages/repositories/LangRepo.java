092363fef1f20d39cb8fb8a4d621b9d4c7bddf35
nramiscal <nramiscal@gmail.com>
2017-09-15 13:37:22 -0400

    added @Repository and import

--- src/main/java/com/nramiscal/languages/repositories/LangRepo.java
+++ src/main/java/com/nramiscal/languages/repositories/LangRepo.java
@@ -1,8 +1,11 @@
 package com.nramiscal.languages.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.nramiscal.languages.models.Language;
 
+@Repository
 public interface LangRepo extends CrudRepository<Language, Long> {
 
 }
